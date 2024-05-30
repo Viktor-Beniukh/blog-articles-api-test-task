6202f6a8388224ebd5a471eba966cbfa832f8a90
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-05-30 16:11:20 +0300

    Added save method to Article model

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,9 @@
+import os
+
 from django.conf import settings
 from django.db import models
+
+from PIL import Image
 
 from articles.utils import articles_picture_file_path
 
@@ -18,3 +22,31 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+        *args,
+        **kwargs,
+    ):
+        try:
+            old_instance = Article.objects.get(pk=self.pk)
+            old_picture = old_instance.picture
+        except Article.DoesNotExist:
+            old_picture = None
+
+        super().save(*args, **kwargs)
+
+        if old_picture and self.picture != old_picture:
+            if os.path.isfile(old_picture.path):
+                os.remove(old_picture.path)
+
+        if self.picture:
+            img = Image.open(self.picture.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.picture.path)
