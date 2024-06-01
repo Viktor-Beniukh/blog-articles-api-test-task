bd969e5925b8fd340a5498402b81d9ceb29180c1
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-01 18:05:33 +0300

    Added extra commands to start_cmd_list.py

--- article_telegram_bot/start_commands_list/start_cmd_list.py
+++ article_telegram_bot/start_commands_list/start_cmd_list.py
@@ -2,5 +2,7 @@ from aiogram.types import BotCommand
 
 
 start_commands = [
+    BotCommand(command="start", description="Welcome message"),
     BotCommand(command="help", description="list of available commands and their descriptions"),
+    BotCommand(command="latest", description="Get the latest article from the blog"),
 ]
