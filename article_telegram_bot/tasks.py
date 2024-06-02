eb1703bcf6467b3cb050a6c8720836b8b98e35c0
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-02 20:40:27 +0300

    Added parse_mode option to bot

--- article_telegram_bot/tasks.py
+++ article_telegram_bot/tasks.py
@@ -11,6 +11,6 @@ logger = logging.getLogger(__name__)
 def send_new_article_notification_task(
     article_id: int, article_title: str, bot_token: str, chat_id: int
 ) -> None:
-    bot = TeleBot(token=bot_token)
+    bot = TeleBot(token=bot_token, parse_mode="HTML")
     message = f"New article published: \n\n<b>#{article_id}. {article_title}</b>"
     bot.send_message(chat_id=chat_id, text=message)
